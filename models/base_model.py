8cb225520d9690df130554a9987ab427e1736a41
Amr Abdelghany <joo_amr91@yahoo.com>
2024-02-19 17:03:19 -0800

    done

--- models/base_model.py
+++ models/base_model.py
@@ -1,1 +1,17 @@
-"hello"
+#!/usr/bin/python3
+    """ This is the base model """
+
+from uuid import uuid4
+from datetime import datetime
+
+Class BaseModel():
+
+    """ The Base Model """
+
+    def 
+
+
+
+
+model_id = str(uuid.uuid4())
+
